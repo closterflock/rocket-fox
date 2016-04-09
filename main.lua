7aa936982a27e507e7b71f47cd5045a6a363bcc6
James Spence <Jamesaspence20@gmail.com>
2016-04-09 16:21:09 -0500

    win condition, text up top

--- main.lua
+++ main.lua
@@ -30,6 +30,9 @@ function love.load(arg)
         acceleration = 20,
     }
 
+    win = false
+    lose = false
+
     startingX = player.x
     startingY = player.y
 
@@ -51,69 +54,123 @@ function love.draw(dt)
     love.graphics.draw(bunnyImg, 900, 500)
     animation:draw(player.image, player.x, player.y, 0, 1, 1, offsetX, offsetY)
 
-    love.graphics.print("X Mouse Position: " .. xmousepos, 200, 20)
-    love.graphics.print("Y Mouse Position: " .. ymousepos, 200, 40)
-    love.graphics.print("End x Pos: " .. mousex, 200, 60)
-    love.graphics.print("End y Pos: " .. mousey, 200, 80)
-    love.graphics.print("Velocity: " .. velocity, 200, 100)
-    love.graphics.print("Angle: " .. angle, 200, 120)
-    love.graphics.print("Xposition: " .. player.x, 350, 20)
-    love.graphics.print("Yposition: " .. player.y, 350, 40)
-    love.graphics.print("Heading  : " .. player.heading, 350, 60)
-
-    love.graphics.print("LaunchVel:  " .. launchvel, 650, 20)
-    love.graphics.print("Yvel:       " .. player.velY, 650, 50)
-    love.graphics.print("xvel:       " .. player.velX, 650, 70)
-    love.graphics.print("degangle:       " .. degangle, 650, 90)
-
-    if jump then
-        love.graphics.print("Jumping", 50, 210)
+    if win then
+      love.graphics.print("KILLED THAT BASTARD", 650, 20, 0, 2)
+    elseif lose then
+      love.graphics.print("Missed, Idiot", 650, 20, 0, 2)
     end
 
-    love.graphics.line(startingX, startingY, mousex, mousey)
+    -- love.graphics.print("X Mouse Position: " .. xmousepos, 200, 20)
+    -- love.graphics.print("Y Mouse Position: " .. ymousepos, 200, 40)
+    -- love.graphics.print("End x Pos: " .. mousex, 200, 60)
+    -- love.graphics.print("End y Pos: " .. mousey, 200, 80)
+    -- love.graphics.print("Velocity: " .. velocity, 200, 100)
+    -- love.graphics.print("Angle: " .. angle, 200, 120)
+    -- love.graphics.print("Xposition: " .. player.x, 350, 20)
+    -- love.graphics.print("Yposition: " .. player.y, 350, 40)
+    -- love.graphics.print("Heading  : " .. player.heading, 350, 60)
+    --
+    -- love.graphics.print("LaunchVel:  " .. launchvel, 650, 20)
+    -- love.graphics.print("Yvel:       " .. player.velY, 650, 50)
+    -- love.graphics.print("xvel:       " .. player.velX, 650, 70)
+    -- love.graphics.print("degangle:       " .. degangle, 650, 90)
+    --
+    -- if jump then
+    --     love.graphics.print("Jumping", 50, 210)
+    -- end
+    --
+    -- love.graphics.line(startingX, startingY, mousex, mousey)
 end
 
 -- Updating
+-- function love.update(dt)
+--     animation:update(dt)
+-- 	-- I always start with an easy way to exit the game
+-- 	if love.keyboard.isDown('escape') then
+-- 		love.event.push('quit')
+-- 	end
+--
+--     xmousepos = love.mouse.getX()
+--     ymousepos = love.mouse.getY()
+--
+--     velocity = (math.dist(offsetX,offsetY,mousex,mousey))
+--     angle = (math.angle(offsetX,offsetY,mousex,mousey))
+--
+--     degangle = math.deg(angle)
+--
+--     if jump then
+--       player.img = love.graphics.newImage('assets/flying.png')
+--
+--       -- radsin = math.sin(angle)
+--       -- degsin = math.deg(radsin)
+--       --
+--       -- radcos = math.cos(angle)
+--       -- degcos = math.deg(radcos)
+--
+--       if firstloop then
+--         player.velY = 3*(startingY - mousey)
+--         player.velX = 2*(mousex - startingX)
+--         firstloop = false
+--       else
+--         player.velY = player.velY + (-1500 * dt)
+--
+--       -- player.velX = (radsin * player.acceleration * dt)
+--     	-- player.velY = (radcos * -player.acceleration * dt)
+--
+--       -- player.velX = (player.velX * player.acceleration * dt)
+--     	-- player.velY = (player.velY * -player.acceleration * dt)
+--       moveFox(dt)
+--    end
+--
+--     end
+-- end
+
 function love.update(dt)
     animation:update(dt)
-	-- I always start with an easy way to exit the game
-	if love.keyboard.isDown('escape') then
-		love.event.push('quit')
-	end
-
+    -- I always start with an easy way to exit the game
+     if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+     end
     xmousepos = love.mouse.getX()
     ymousepos = love.mouse.getY()
-
     velocity = (math.dist(offsetX,offsetY,mousex,mousey))
     angle = (math.angle(offsetX,offsetY,mousex,mousey))
-
     degangle = math.deg(angle)
-
     if jump then
       player.img = love.graphics.newImage('assets/flying.png')
-
-      -- radsin = math.sin(angle)
-      -- degsin = math.deg(radsin)
-      --
-      -- radcos = math.cos(angle)
-      -- degcos = math.deg(radcos)
-
-      if firstloop then
-        player.velY = 3*(startingY - mousey)
-        player.velX = 2*(mousex - startingX)
-        firstloop = false
-      else
+      if looping then
         player.velY = player.velY + (-1500 * dt)
-
-      -- player.velX = (radsin * player.acceleration * dt)
-    	-- player.velY = (radcos * -player.acceleration * dt)
-
-      -- player.velX = (player.velX * player.acceleration * dt)
-    	-- player.velY = (player.velY * -player.acceleration * dt)
-      moveFox(dt)
+        moveFox(dt)
+        if player.y > 550 then
+          looping = false
+          done = true
+        end
+      end
+      if firstloop then
+          player.velY = 3*(startingY - mousey)
+          player.velX = 3*(mousex - startingX)
+          startingVelY = player.velY
+          if player.velX < 800 then
+            player.velX = player.velX
+          elseif player.velX > 800 then
+            player.velX = 800
+          end
+          if player.velY < 1000 then
+            player.velY = player.velY
+          elseif player.velY > 1000 then
+            player.velY = 1000
+          end
+          firstloop = false
+          looping = true
+     end
    end
-
+   if done then
+     if player.x > 800 and player.x < 1050 then
+       win = true
+     else
+      lose = true
     end
+  end
 end
 
 function moveFox(dt)
@@ -125,6 +182,7 @@ end
 --gets the position of mouse release
 function love.mousereleased(x, y, button)
    if button == 1 then
+       print('MOUSE RELEASE')
       mousex = x
       mousey = y
       jump = true
