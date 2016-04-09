25696ade221a3d146b881bb9551499f6bd158e0e
Carter <carter.egan@gmail.com>
2016-04-09 14:48:37 -0500

    made it super lfy

--- main.lua
+++ main.lua
@@ -1,36 +1,42 @@
-
-
 function love.load(arg)
 
-
-    width = love.graphics.getWidth()
-    height = love.graphics.getHeight()
+  width = love.graphics.getWidth()
+  height = love.graphics.getHeight()
 
 --for the player class
-player = { x = width - (width - 0), y = height - (height - 500), acceleration = 1, velY = 1, velX = 1, img = nil}
+--player = { x = width - (width - 0), y = height - (height - 500), acceleration = 200, velY = 1, velX = 1, img = nil}
+
+-- loads the two backgrounds and the player
+backgroundimg = love.graphics.newImage('assets/background.png')
+foregroundimg = love.graphics.newImage('assets/fore.png')
+
+playerimg = love.graphics.newImage('assets/sitting.png')
+
+offsetX = playerimg:getWidth() / 2
+offsetY = playerimg:getHeight() / 2
+
+player = {
+  x = width - (width - 0) + offsetX,
+  y = height - (height - 500) + offsetY,
+  img = playerimg,
+  heading = 0,
+  velX = 1,
+  velY = 1,
+  acceleration = 20,
+}
 
 startingX = player.x
 startingY = player.y
 
--- loads the two backgrounds and the player
-player.img = love.graphics.newImage('assets/sitting.png')
-    backgroundimg = love.graphics.newImage('assets/background.png')
-    foregroundimg = love.graphics.newImage('assets/fore.png')
 --set the count
  --set the count
-    mousepos = 0
-    jump = false
-    firstloop = true
-    launchvel = 0
-    mousex = 1
-    mousey = 1
-
-
-  decay = 150
-  terminalVelocity = 400
-  width = love.graphics.getWidth()
-  height = love.graphics.getHeight()
-
+mousepos = 0
+jump = false
+firstloop = true
+launchvel = 0
+mousex = 1
+mousey = 1
+angledeg = 0
 
 end
 
@@ -38,20 +44,23 @@ end
 function love.draw(dt)
     love.graphics.draw(backgroundimg, 0, 0)
     love.graphics.draw(foregroundimg, 0, 560)
-    love.graphics.draw(player.img, player.x, player.y)
+    love.graphics.draw(player.img, player.x, player.y, player.heading, 1, 1, offsetX, offsetY)
 
-    love.graphics.print("X Mouse Position: " .. xmousepos, 50, 50)
-    love.graphics.print("Y Mouse Position: " .. ymousepos, 50, 70)
-    love.graphics.print("End x Pos: " .. mousex, 50, 90)
-    love.graphics.print("End y Pos: " .. mousey, 50, 110)
-    love.graphics.print("Velocity: " .. velocity, 50, 130)
-    love.graphics.print("Angle: " .. angle, 50, 150)
-    love.graphics.print("Xposition: " .. player.x, 50, 170)
-    love.graphics.print("Yposition: " .. player.y, 50, 190)
 
-    love.graphics.print("LaunchVel:  " .. launchvel, 450, 50)
-    love.graphics.print("Yvel:       " .. player.velY, 450, 70)
-    love.graphics.print("xvel:       " .. player.velX, 450, 90)
+    love.graphics.print("X Mouse Position: " .. xmousepos, 200, 20)
+    love.graphics.print("Y Mouse Position: " .. ymousepos, 200, 40)
+    love.graphics.print("End x Pos: " .. mousex, 200, 60)
+    love.graphics.print("End y Pos: " .. mousey, 200, 80)
+    love.graphics.print("Velocity: " .. velocity, 200, 100)
+    love.graphics.print("Angle: " .. angle, 200, 120)
+    love.graphics.print("Xposition: " .. player.x, 350, 20)
+    love.graphics.print("Yposition: " .. player.y, 350, 40)
+    love.graphics.print("Heading  : " .. player.heading, 350, 60)
+
+    love.graphics.print("LaunchVel:  " .. launchvel, 650, 20)
+    love.graphics.print("Yvel:       " .. player.velY, 650, 50)
+    love.graphics.print("xvel:       " .. player.velX, 650, 70)
+    love.graphics.print("degangle:       " .. degangle, 650, 90)
 
     if jump then
     love.graphics.print("Jumping", 50, 210)
@@ -70,50 +79,43 @@ function love.update(dt)
     xmousepos = love.mouse.getX()
     ymousepos = love.mouse.getY()
 
-    velocity = (math.dist(startingX,startingY,mousex,mousey))/500000
-    angle = -1*(math.angle(startingX,startingY,mousex,mousey))
+    velocity = (math.dist(offsetX,offsetY,mousex,mousey))
+    angle = (math.angle(offsetX,offsetY,mousex,mousey))
+
+    degangle = math.deg(angle)
 
     if jump then
-
       player.img = love.graphics.newImage('assets/flying.png')
 
-      player.velX = -1*(math.sin(angle) * player.acceleration * dt)
-    	player.velY = -1*(math.cos(angle) * -player.acceleration * dt)
+      -- radsin = math.sin(angle)
+      -- degsin = math.deg(radsin)
+      --
+      -- radcos = math.cos(angle)
+      -- degcos = math.deg(radcos)
 
-      --terminalVelocity
-    	-- if player.velY ~= 0 and player.velY < terminalVelocity then
-    	-- 	player.velY = player.velY + (decay * dt)
-    	-- end
-    	-- if player.velY > terminalVelocity then
-    	-- 	player.velY = terminalVelocity
-    	-- end
-    	--if player.y >= 500 then
-    		--player.y = 500
-    		--player.velY = 0
-    		--player.velX = 0
-    	--end
+      if firstloop then
+        player.velY = 3*(startingY - mousey)
+        player.velX = 2*(mousex - startingX)
+        firstloop = false
+      else
+        player.velY = player.velY + (-1500 * dt)
 
-        moveFox(dt)
+      -- player.velX = (radsin * player.acceleration * dt)
+    	-- player.velY = (radcos * -player.acceleration * dt)
+
+      -- player.velX = (player.velX * player.acceleration * dt)
+    	-- player.velY = (player.velY * -player.acceleration * dt)
+      moveFox(dt)
+   end
+
     end
 end
 
 function moveFox(dt)
     player.x = player.x + player.velX * dt
-    player.y = (player.y + player.velY * dt)
+    player.y = (player.y - player.velY * dt)
 
-    -- if player.x > width then
-    --     player.x = 0
-    -- elseif player.x < 0 then
-    --     player.x = width
-    -- end
-    -- if player.y > height then
-    --     player.y = 0
-    -- elseif player.y < 0 then
-    --     player.y = height
-    -- end
 end
-
-
 
 --gets the position of mouse release
 function love.mousereleased(x, y, button)
@@ -123,8 +125,6 @@ function love.mousereleased(x, y, button)
      jump = true
    end
 end
-
-
 
 function math.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
 
