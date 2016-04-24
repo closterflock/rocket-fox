c43c3a8d5a178a1cb96875fc6d97d2277e602fb9
James Spence <Jamesaspence20@gmail.com>
2016-04-23 23:23:57 -0500

    player is now fully separated

--- main.lua
+++ main.lua
@@ -90,49 +90,6 @@ function love.update(dt)
     end
 
     movebunnys(dt)
-
-    xmousepos = love.mouse.getX()
-    ymousepos = love.mouse.getY()
-    -- velocity = (math.dist(offsetX,offsetY,mousex,mousey))
-    -- angle = (math.angle(offsetX,offsetY,mousex,mousey))
-    -- degangle = math.deg(angle)
-
-    if flying then
-        player.heading = getHeadingOfFox(player.x, player.y, xmousepos, ymousepos)
-        player.img = love.graphics.newImage('assets/flying.png')
-        if love.mouse.isDown(1) then
-            boostTowardsMouse(xmousepos, ymousepos, dt)
-        end
-        if looping then
-            player.velY = player.velY + (-500 * dt)
-            moveFox(dt)
-            if player.y > 550 then
-                looping = false
-                done = true
-            end
-        end
-        if firstloop then
-            player.velY = -5*(player.getStartingY() - mousey)
-            player.velX = -5*(mousex - player.getStartingX())
-            startingVelY = player.velY
-            if player.velX > -800 then
-                player.velX = 1*player.velX
-            elseif player.velX < -800 then
-                player.velX = 800
-            end
-            if player.velY < 1000 then
-                player.velY = player.velY
-            elseif player.velY > 1000 then
-                player.velY = 1000
-            end
-            firstloop = false
-            looping = true
-        end
-    end
-    if done then
-        flying = false
-        done = true
-    end
 end
 
 function movebunnys(dt)
@@ -171,60 +128,10 @@ function movebunnys(dt)
     end
 end
 
-function moveFox(dt)
-    player.x = player.x + player.velX * dt
-    player.y = (player.y - player.velY * dt)
-end
-
---gets the position of mouse release
 function love.mousereleased(x, y, button)
-    if button == 1 then
-        if slingshot then
-            mousex = x
-            mousey = y
-            slingshot = false
-            flying = true
-        end
-    end
+    player:mouseReleased(x, y, button)
 end
 
 function love.mousepressed(x, y, button, istouch)
-    if button == 1 then -- the primary button
-        if standing then
-            if xmousepos > player.x - 250 and xmousepos < player.x + 250 and ymousepos > 460 and ymousepos < 560 then
-                standing = false
-                slingshot = true
-            end
-        end
-    end
-end
-
-function boostTowardsMouse(mouseX, mouseY, dt)
-    player.velX = player.velX + math.cos(player.heading) * player.acceleration * dt
-    player.velY = player.velY - math.sin(player.heading) * player.acceleration * dt
-end
-
-function getHeadingOfFox(playerX, playerY, mouseX, mouseY)
-    local yDiff = mouseY - playerY
-    local xDiff = mouseX - playerX
-    local heading = math.atan(yDiff / xDiff)
-
-    if xDiff < 0 then
-        local radiansToAdd = math.rad(90)
-        if yDiff < 0 and heading > 0 then
-            heading = (radiansToAdd * -1) - (radiansToAdd - heading)
-        elseif yDiff > 0 and heading < 0 then
-            heading = (radiansToAdd + (radiansToAdd + heading))
-        end
-    end
-
-    return heading, yDiff, xDiff
-end
-
-function math.angle(x1,y1, x2,y2)
-    return math.atan2(y2-y1, x2-x1)
-end
-
-function math.dist(x1,y1, x2,y2)
-        return ((x2-x1)^2+(y2-y1)^2)^0.5
+    player:mousePressed(x, y, button)
 end
--- player.lua
+++ player.lua
@@ -29,30 +29,141 @@ local player = {
     velX = 1,
     velY = 1,
     acceleration = 750,
+    standing = true,
+    slingshot = false,
+    flying = false
 }
-
-function player:getStartingPosition()
-    return self:getStartingX(), self:startingY()
-end
-
-function player.getStartingX()
-    return starting.x
-end
-
-function player.getStartingY()
-    return starting.y
-end
 
 function player:setIdleAnimation()
     self.animation = idleAnimation
+end
+
+function player:setFlying()
+    self.flying = true
 end
 
 function player:draw()
     self.animation:draw(self.image, self.x, self.y, self.heading, 1, 1, offsets.x, offsets.y)
 end
 
+function player:updateHeading(mouseX, mouseY)
+local xDiff = mouseX - self.x
+    local yDiff = mouseY - self.y
+    local heading = math.atan(yDiff / xDiff)
+
+    if xDiff < 0 then
+        local radiansToAdd = math.rad(90)
+        if yDiff < 0 and heading > 0 then
+            heading = (radiansToAdd * -1) - (radiansToAdd - heading)
+        elseif yDiff > 0 and heading < 0 then
+            heading = (radiansToAdd + (radiansToAdd + heading))
+        end
+    end
+
+    self.heading = heading
+end
+
 function player:update(dt)
     self.animation:update(dt)
+    if self.flying then
+        self:movePlayer(dt)
+        local mouseX = love.mouse.getX()
+        local mouseY = love.mouse.getY()
+        self:updateHeading(mouseX, mouseY)
+        self:decayY(dt)
+        self:checkForGround()
+        if love.mouse.isDown(1) then
+            player:boostTowardsMouse(mouseX, mouseY, dt)
+        end
+    end
+end
+
+function player:checkForGround()
+    if self.y > 550 then
+        self.y = 550
+        self:stopPlayer()
+    end
+end
+
+function player:decayY(dt)
+    if self.velY < 1000 then
+        self.velY = self.velY - (dt * 500)
+    elseif self.velY > 1000 then
+        self.velY = 1000
+    end
+end
+
+function player:onGround()
+    return self:animationIs(idleAnimation)
+end
+
+function player:animationIs(animation)
+    return (self.animation == animation)
+end
+
+function player:stopPlayer()
+    self.velX = 0
+    self.velY = 0
+    self.flying = false
+    self.standing = true
+    self.heading = 0
+    self:setIdleAnimation()
+    print('standing mode')
+end
+
+function player:movePlayer(dt)
+    self.x = self.x + self.velX * dt
+    self.y = (self.y - self.velY * dt)
+end
+
+function player:initialJump(mouseX, mouseY)
+    self.velY = -5*(self.y - mouseY)
+    self.velX = -5*(mouseX - self.x)
+    self.flying = true
+    print(self.velY)
+    print(self.velX)
+end
+
+function player:mouseReleased(mouseX, mouseY, button)
+    if button == 1 then
+        if self.slingshot then
+            print('flying mode')
+            self:initialJump(mouseX, mouseY)
+            self.slingshot = false
+            self.flying = true
+        end
+    end
+end
+
+function player:mousePressed(mouseX, mouseY, button)
+    if button == 1 then
+        if self.standing then
+            if self:pointIntersectsPlayer(mouseX, mouseY) then
+                print('slingshot mode')
+                self.standing = false
+                self.slingshot = true
+            end
+        end
+    end
+end
+
+function player:getHitBoxSides()
+    local negativeX = self.x - frameSize.x
+    local positiveX = self.x + frameSize.x
+    local negativeY = self.y - frameSize.y
+    local positiveY = self.y + frameSize.y
+
+    return negativeX, positiveX, negativeY, positiveY
+end
+
+function player:pointIntersectsPlayer(x, y)
+    local negativeX, positiveX, negativeY, positiveY = self:getHitBoxSides()
+    return (x > negativeX and x < positiveX) and (y > negativeY and y < positiveY)
+end
+
+function player:boostTowardsMouse(mouseX, mouseY, dt)
+    self.velX = self.velX + math.cos(self.heading) * self.acceleration * dt
+    self.velY = self.velY - math.sin(self.heading) * self.acceleration * dt
 end
 
 return player
